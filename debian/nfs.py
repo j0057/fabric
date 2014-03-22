89f8127e32b7973c8f7c7133cf0b5bcc61471af1
Joost Molenaar <j.j.molenaar@gmail.com>
2014-03-22 18:56:25 +0100

    NFS config

--- debian/nfs.py
+++ debian/nfs.py
@@ -14,8 +14,20 @@ def nfs_install():
     'Install NFS'
     deb.packages(['nfs-kernel-server'])
 
+@task
+@roles('debian-nfs')
+def nfs_configure():
+    with watch('/etc/exports') as exports:
+        for n in env.config[env.host_string]['nfs']['exports']:
+            line = '{0:16} {1}({2})'.format(n['path'], n['net'], ','.join(n['options']))
+            if not contains('/etc/exports', line):
+                append('/etc/exports', line)
+    if exports.changed:
+        run('systemctl restart nfs-kernel-server')
+
 @task(default=True)
 @roles('debian-nfs')
 def nfs():
     'Do all the NFS things'
     execute(nfs_install)
+    execute(nfs_configure)
